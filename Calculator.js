9850ea1c16a2e9d5198b6aabd748167a8c13d6e0
David Gaona <davidgaonahenao@gmail.com>
2023-09-12 19:29:25 -0500

    The Second Function Of JavaScript Is Created

--- Calculator.js
+++ Calculator.js
@@ -1,3 +1,7 @@
 function sumar (a, b) {
-    console.log("La suma de", a, "y", b, a + b);
+    console.log("La suma de", a, "y", b, "es:", a + b);
+}
+
+function restar (a, b) {
+    console.log("La resta de", a, "y", b, "es:", a -b);
 }
